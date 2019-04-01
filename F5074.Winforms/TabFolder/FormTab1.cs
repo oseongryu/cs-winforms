486e6973af14df5bfbd174a194e6e426bad5e802
f5074 <oseongryu@gmail.com>
2019-04-01 12:38:34 +0900

    Make Prameters

--- F5074.Winforms/TabFolder/FormTab1.cs
+++ F5074.Winforms/TabFolder/FormTab1.cs
@@ -27,6 +27,11 @@ namespace F5074.Winforms.TabFolder
         public FormTab1()
         {
             InitializeComponent();
+            MakeParameters();
+        }
+
+        private void MakeSentences()
+        {
             string a = "this.txt";
             string b = ".EditValue = ";
             string d = ";";
@@ -34,11 +39,29 @@ namespace F5074.Winforms.TabFolder
             for (int x = 0; x < Enum.GetNames(typeof(DBIndex)).Length; x++)
             {
                 string c = Enum.GetNames(typeof(DBIndex))[x].ToString();
-                this.textBox1.AppendText( a + c + b + c + d + "\r\n");
+                this.textBox1.AppendText(a + c + b + c + d + "\r\n");
                 //this.textBox1.Text += a + c + b + c + d + "\r\n";
                 //Console.WriteLine(a + c + b + c + d);
             }
         }
+
+        private void MakeParameters()
+        {
+            // https://docs.microsoft.com/ko-kr/dotnet/framework/winforms/controls/how-to-put-quotation-marks-in-a-string-windows-forms
+            string a = "parameters.Add(\u0022";
+            string b = "\u0022, \u0022\u0022)";
+            string d = ";";
+
+            for (int x = 0; x < Enum.GetNames(typeof(DBIndex)).Length; x++)
+            {
+                string c = Enum.GetNames(typeof(DBIndex))[x].ToString();
+                this.textBox1.AppendText(a + c + b + d + "\r\n");
+                //this.textBox1.Text += a + c + b + c + d + "\r\n";
+                //Console.WriteLine(a + c + b + c + d);
+            }
+        }
+
+
 
     }
 }
