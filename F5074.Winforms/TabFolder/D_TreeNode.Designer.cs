4e4ab7b6e2012cc70a2e555291df9b469018cd59
f5074 <oseongryu@gmail.com>
2019-04-03 09:30:00 +0900

    Winforms TreeView

---
+++ F5074.Winforms/TabFolder/D_TreeNode.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace F5074.Winforms.TabFolder
+{
+    partial class D_TreeNode
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(38, 37);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(233, 361);
+            this.treeView1.TabIndex = 0;
+            // 
+            // D_TreeNode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.treeView1);
+            this.Name = "D_TreeNode";
+            this.Size = new System.Drawing.Size(710, 450);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ F5074.Winforms/TabFolder/D_TreeNode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.Winforms.TabFolder
+{
+    public partial class D_TreeNode : UserControl
+    {
+        public D_TreeNode()
+        {
+            InitializeComponent();
+            InitializeTreeView();
+        }
+
+        // https://m.blog.naver.com/PostView.nhn?blogId=chsmanager&logNo=140209835137&proxyReferer=https%3A%2F%2Fwww.google.com%2F
+        private void InitializeTreeView()
+        {
+            List<CodeValueEntity> menuFactorys = new List<CodeValueEntity>() { new CodeValueEntity() { Code = "A", Value = "A" }, new CodeValueEntity() { Code = "B", Value = "B" } };
+            List<GroupCodeValueEntity> scanners = new List<GroupCodeValueEntity>() { new GroupCodeValueEntity() { Group = "A", Code = "AA", Value = "AA" }, new GroupCodeValueEntity() { Group = "A", Code = "AB", Value = "AB" } };
+            TreeNode tNode = new TreeNode();
+            foreach (CodeValueEntity menuFactory in menuFactorys)
+            {
+                if (menuFactory.Code == "") continue;
+                TreeNode node = getNodeSecond(getNodeFirst(menuFactory), scanners.Where(x => x.Group == menuFactory.Code).ToList());
+                treeView1.Nodes.Add(node);
+            }
+        }
+
+        private TreeNode getNodeFirst(CodeValueEntity menuFactory)
+        {
+            TreeNode node = new TreeNode();
+            node.Text = menuFactory.Value;
+            node.Tag = menuFactory.Code;
+            node.Name = "m" + menuFactory.Code;
+            return node;
+        }
+
+        private TreeNode getNodeSecond(TreeNode parentNode, List<GroupCodeValueEntity> scanners)
+        {
+            foreach (GroupCodeValueEntity scanner in scanners)
+            {
+                TreeNode node = new TreeNode();
+                node.Text = scanner.Value;
+                node.Tag = scanner.Code;
+                node.Name = "m" + scanner.Group + "_s" + scanner.Code;
+                parentNode.Nodes.Add(node);
+            }
+            return parentNode;
+        }
+
+
+        public class CodeValueEntity
+        {
+            public string Code { get; set; }
+            public string Value { get; set; }
+        }
+
+        public class GroupCodeValueEntity
+        {
+            public string Group { get; set; }
+            public string Code { get; set; }
+            public string Value { get; set; }
+            public string Etc1 { get; set; }
+        }
+    }
+}
