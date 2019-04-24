541b6cda1bf960f1ea418b342c2433d4f63d503c
f5074 <oseongryu@gmail.com>
2019-04-25 01:41:16 +0900

    Add PdfViewer

---
+++ F5074.DevExpressWinforms/MyForm/H_PdfViewer/MyPdfViewer01.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace F5074.DevExpressWinforms.MyForm.H_PdfViewer
+{
+    partial class MyPdfViewer01
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pdfViewer1 = new DevExpress.XtraPdfViewer.PdfViewer();
+            this.SuspendLayout();
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfViewer1.Location = new System.Drawing.Point(0, 0);
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.Size = new System.Drawing.Size(1130, 561);
+            this.pdfViewer1.TabIndex = 0;
+            // 
+            // MyPdfViewer01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pdfViewer1);
+            this.Name = "MyPdfViewer01";
+            this.Size = new System.Drawing.Size(1130, 561);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraPdfViewer.PdfViewer pdfViewer1;
+    }
+}
---
+++ F5074.DevExpressWinforms/MyForm/H_PdfViewer/MyPdfViewer01.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace F5074.DevExpressWinforms.MyForm.H_PdfViewer
+{
+    public partial class MyPdfViewer01 : UserControl
+    {
+        public MyPdfViewer01()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPDF()
+        {
+            try
+            {
+                MemoryStream ms;
+                using (WebClient client = new WebClient())
+                {
+                    Uri _pdfUri = new Uri(@"http://kostat.go.kr/file_total/kor3/korIp1_14.pdf");
+                    byte[] fileBytes = client.DownloadData(_pdfUri);
+                    ms = new MemoryStream(fileBytes);
+                    pdfViewer1.LoadDocument(ms);
+                }
+
+                //string url = @"http://kostat.go.kr/file_total/kor3/korIp1_14.pdf";
+                //MemoryStream stream;
+                //WebClient wc = new WebClient();
+                //byte[] data = wc.DownloadData(url);
+                //stream = new MemoryStream(data);
+                //pdfViewer1.LoadDocument(stream);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
