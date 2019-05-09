488a0ad2fc6fc5820631c7f2b57c98b87c092fb4
f5074 <oseongryu@gmail.com>
2019-05-09 13:56:11 +0900

    Add FlyoutPanel

---
+++ F5074.DevExpressWinforms/MyForm/G_CommonControls/MyFlyoutPanel01.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace F5074.DevExpressWinforms.MyForm.G_CommonControls
+{
+    partial class MyFlyoutPanel01
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.flyoutPanel2 = new DevExpress.Utils.FlyoutPanel();
+            this.flyoutPanelControl1 = new DevExpress.Utils.FlyoutPanelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.flyoutPanel2)).BeginInit();
+            this.flyoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.flyoutPanelControl1)).BeginInit();
+            this.flyoutPanelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(92, 162);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(156, 126);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "simpleButton1";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // flyoutPanel2
+            // 
+            this.flyoutPanel2.Controls.Add(this.flyoutPanelControl1);
+            this.flyoutPanel2.Location = new System.Drawing.Point(190, 3);
+            this.flyoutPanel2.Name = "flyoutPanel2";
+            this.flyoutPanel2.Options.CloseOnOuterClick = true;
+            this.flyoutPanel2.Size = new System.Drawing.Size(546, 138);
+            this.flyoutPanel2.TabIndex = 3;
+            // 
+            // flyoutPanelControl1
+            // 
+            this.flyoutPanelControl1.Controls.Add(this.labelControl1);
+            this.flyoutPanelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flyoutPanelControl1.FlyoutPanel = this.flyoutPanel2;
+            this.flyoutPanelControl1.Location = new System.Drawing.Point(0, 0);
+            this.flyoutPanelControl1.Name = "flyoutPanelControl1";
+            this.flyoutPanelControl1.Size = new System.Drawing.Size(546, 138);
+            this.flyoutPanelControl1.TabIndex = 0;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.AllowHtmlString = true;
+            this.labelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelControl1.Location = new System.Drawing.Point(2, 2);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(358, 81);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "<Size=50>Flyout panel</Size>";
+            // 
+            // MyFlyoutPanel01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flyoutPanel2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "MyFlyoutPanel01";
+            this.Size = new System.Drawing.Size(934, 542);
+            ((System.ComponentModel.ISupportInitialize)(this.flyoutPanel2)).EndInit();
+            this.flyoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.flyoutPanelControl1)).EndInit();
+            this.flyoutPanelControl1.ResumeLayout(false);
+            this.flyoutPanelControl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.Utils.FlyoutPanel flyoutPanel2;
+        private DevExpress.Utils.FlyoutPanelControl flyoutPanelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
---
+++ F5074.DevExpressWinforms/MyForm/G_CommonControls/MyFlyoutPanel01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.DevExpressWinforms.MyForm.G_CommonControls
+{
+    public partial class MyFlyoutPanel01 : UserControl
+    {
+        public MyFlyoutPanel01()
+        {
+            InitializeComponent();
+            //flyoutPanel1.ShowPopup();
+            // https://www.devexpress.com/Support/Center/Question/Details/T333871/flyoutpanel-sample
+            //flyoutPanel1.OwnerControl = this.simpleButton1;
+
+            this.flyoutPanel2.OwnerControl = this;
+            this.flyoutPanel2.Showing += FlyoutPanel2_Showing;
+            this.flyoutPanel2.Hidden += FlyoutPanel2_Hidden;
+        }
+
+        private void FlyoutPanel2_Hidden(object sender, DevExpress.Utils.FlyoutPanelEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void FlyoutPanel2_Showing(object sender, DevExpress.Utils.FlyoutPanelEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            flyoutPanel2.ShowPopup();
+        }
+    }
+}
