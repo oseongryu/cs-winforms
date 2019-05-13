a435fe679180cb45b8a7531203f71acb1048f61f
f5074 <oseongryu@gmail.com>
2019-05-13 16:42:58 +0900

    Use SendKeys

---
+++ F5074.Winforms/MyForm/D_Common/MySendKey01.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace F5074.Winforms.MyForm.D_Common
+{
+    partial class MySendKey01
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(489, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MySendKey01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MySendKey01";
+            this.Size = new System.Drawing.Size(870, 511);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ F5074.Winforms/MyForm/D_Common/MySendKey01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.Winforms.MyForm.D_Common
+{
+    public partial class MySendKey01 : UserControl
+    {
+        public MySendKey01()
+        {
+            InitializeComponent();
+            this.button1.Click += Button1_Click;
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // https://stackoverflow.com/questions/15621147/how-simulate-ctrlv-keystrokes-paste-using-c-sharp
+            // https://11cc.tistory.com/8
+            this.textBox1.Focus();
+            SendKeys.Send("aaaaa");
+            SendKeys.Send("^(a)");
+            SendKeys.Send("^(c)");
+            SendKeys.Send("^(v)");
+            SendKeys.Send("^(v)");
+        }
+    }
+}
