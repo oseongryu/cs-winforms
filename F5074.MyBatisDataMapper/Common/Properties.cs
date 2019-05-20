c02110888a1e7ff4b75f8c2456710b9f4563e571
f5074 <oseongryu@gmail.com>
2019-05-20 19:47:28 +0900

    Add MyBatisNet Class Library

---
+++ F5074.MyBatisDataMapper/Common/Properties.cs
@@ -0,0 +1,31 @@
+﻿using IBatisNet.DataMapper;
+using IBatisNet.DataMapper.SessionStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F5074.MyBatisDataMapper.Common
+{
+    public class Properties
+    {
+        public static ISqlMapper EntityMapper
+        {
+            get
+            {
+                try
+                {
+                    ISqlMapper mapper = Mapper.Instance();
+                    mapper.SessionStore = new HybridWebThreadSessionStore(mapper.Id);
+                    mapper.DataSource.ConnectionString = "User Id=hr;Password=hr;Data Source=(DESCRIPTION=(ADDRESS_LIST= (ADDRESS=(PROTOCOL=TCP) (HOST=127.0.0.1) (PORT=1521))) (CONNECT_DATA = (SERVICE_NAME = XE)))";
+                    return mapper;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
---
+++ F5074.MyBatisDataMapper/Service/Dashboard/DashboardDAO.cs
@@ -0,0 +1,19 @@
+﻿using F5074.MyBatisDataMapper.Common;
+using IBatisNet.DataMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F5074.MyBatisDataMapper.Service.Dashboard
+{
+    public class DashboardDAO
+    {
+        private static ISqlMapper mapper = Properties.EntityMapper;
+        public static IList<DashboardVo> SelectList(DashboardVo vo)
+        {
+            return mapper.QueryForList<DashboardVo>("SelectList", vo);
+        }
+    }
+}
