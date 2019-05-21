2ae07aca63e08a4a581c720b1633a3c38046a78f
f5074 <oseongryu@gmail.com>
2019-05-21 14:09:20 +0900

    Change Settings

--- F5074.MyBatisDataMapper/Common/DBConnection.cs
+++ F5074.MyBatisDataMapper/Common/DBConnection.cs
@@ -1,4 +1,6 @@
-﻿using IBatisNet.DataMapper;
+﻿using IBatisNet.Common.Utilities;
+using IBatisNet.DataMapper;
+using IBatisNet.DataMapper.Configuration;
 using IBatisNet.DataMapper.SessionStore;
 using System;
 using System.Collections.Generic;
@@ -6,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace F5074.MyBatisDataMapper.Common
 {
@@ -17,7 +20,16 @@ namespace F5074.MyBatisDataMapper.Common
             {
                 try
                 {
-                    ISqlMapper mapper = Mapper.Instance();
+                    // 1. root에 SqlMap.config일 경우
+                    //ISqlMapper mapper = Mapper.Instance();
+
+                    // 2. Config의 위치 변경일 경우
+                    // https://guyaga.tistory.com/423
+                    DomSqlMapBuilder dom = new DomSqlMapBuilder();
+                    XmlDocument sqlMapConfig = Resources.GetEmbeddedResourceAsXmlDocument("Config.SqlMap.config, F5074.MyBatisDataMapper");
+                    ISqlMapper mapper = dom.Configure(sqlMapConfig);
+
+
                     mapper.SessionStore = new HybridWebThreadSessionStore(mapper.Id);
                     mapper.DataSource.ConnectionString = "User Id=hr;Password=hr;Data Source=(DESCRIPTION=(ADDRESS_LIST= (ADDRESS=(PROTOCOL=TCP) (HOST=127.0.0.1) (PORT=1521))) (CONNECT_DATA = (SERVICE_NAME = XE)))";
 
@@ -35,5 +47,6 @@ namespace F5074.MyBatisDataMapper.Common
                 }
             }
         }
+
     }
 }
