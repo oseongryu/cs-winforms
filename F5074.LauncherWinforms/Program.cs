faaedd6b515c225510d2df4daf259995dabe3321
f5074 <oseongryu@gmail.com>
2019-05-25 11:39:36 +0900

    Add Launcher WInforms

---
+++ F5074.LauncherWinforms/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.LauncherWinforms
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DevExpress.XtraEditors.WindowsFormsSettings.DefaultFont = new Font("맑은 고딕", 9);
+            DevExpress.XtraEditors.WindowsFormsSettings.DefaultMenuFont = new Font("맑은 고딕", 9);
+            DevExpress.XtraEditors.WindowsFormsSettings.DefaultPrintFont = new Font("맑은 고딕", 9);
+
+            //This set the style to use skin technology
+            DevExpress.LookAndFeel.UserLookAndFeel.Default.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Skin;
+            //Here we specify the skin to use by its name           
+            //DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Office 2013");
+            //DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Office 2016 Black");
+            //DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Office 2016 Colorful");
+            //DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Metropolis Dark");
+            //BonusSkins.Register();
+            //SkinManager.EnableFormSkins();
+            //DevExpress.LookAndFeel.UserLookAndFeel.Default.SetStyle(LookAndFeelStyle.Skin, false, false, "Metropolis Dark");
+
+
+            //DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Office 2016 Colorful");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm() { WindowState = FormWindowState.Maximized });
+        }
+    }
+}
