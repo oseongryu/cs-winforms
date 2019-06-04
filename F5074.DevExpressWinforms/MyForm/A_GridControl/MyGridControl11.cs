4594ec23df704955478d29ab637802a8b2b22d25
f5074 <oseongryu@gmail.com>
2019-06-04 10:34:37 +0900

    -

---
+++ F5074.DevExpressWinforms/MyForm/A_GridControl/MyGridControl11.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.DevExpressWinforms.MyForm.A_GridControl
+{
+    public partial class MyGridControl11 : UserControl
+    {
+        public MyGridControl11()
+        {
+            InitializeComponent();
+            this.gridControl1.DataSource = CreateTable(10);
+        }
+
+        private DataTable CreateTable(int RowCount)
+        {
+            Random rnd = new Random();
+            DataTable tbl = new DataTable();
+
+            tbl.Columns.Add("Checked", typeof(string));
+            tbl.Columns.Add("Count", typeof(int));
+            tbl.Columns.Add("Name", typeof(string));
+            tbl.Columns.Add("Age", typeof(int));
+            tbl.Columns.Add("Height", typeof(int));
+
+            //tbl.Columns.Add("Checked", typeof(bool));
+            for (int i = 0; i < RowCount; i++)
+                tbl.Rows.Add(new object[] { "False", i, "kim", (10 + i), (70 + i) });
+            return tbl;
+        }
+
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- F5074.LauncherWPF/View/MainHamburger.xaml.cs
+++ F5074.LauncherWPF/View/MainHamburger.xaml.cs
@@ -34,17 +34,17 @@ namespace F5074.LauncherWPF.View
             //layoutPanel1.Content = host;
             //dockLayoutManager1.DockController.Dock(layoutPanel1, layoutGroup1, DockType.Fill);
             LayoutPanel layoutPanel1 = dockLayoutManager1.DockController.AddPanel(DockType.None);
-            layoutPanel1.Content = new FlowLayout01();
+            layoutPanel1.Content = new UserControl();
             layoutPanel1.Caption = "대시보드1";
             dockLayoutManager1.DockController.Insert(documentGroup1, layoutPanel1, 0);
 
             layoutPanel1 = dockLayoutManager1.DockController.AddPanel(DockType.Fill);
-            layoutPanel1.Content = new FlowLayout01();
+            layoutPanel1.Content = new UserControl();
             layoutPanel1.Caption = "대시보드2";
             dockLayoutManager1.DockController.Insert(documentGroup1, layoutPanel1, 1);
 
             layoutPanel1 = dockLayoutManager1.DockController.AddPanel(DockType.Fill);
-            layoutPanel1.Content = new FlowLayout01();
+            layoutPanel1.Content = new UserControl();
             layoutPanel1.Caption = "대시보드3";
             dockLayoutManager1.DockController.Insert(documentGroup1, layoutPanel1,2);
 
