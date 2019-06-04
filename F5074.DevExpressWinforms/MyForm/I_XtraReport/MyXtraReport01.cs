2e7b474387b8fd3fd14c47d4e73bd9540e884916
f5074 <oseongryu@gmail.com>
2019-06-04 16:18:01 +0900

    -

--- F5074.DevExpressWinforms/MyForm/I_XtraReport/MyXtraReport01.cs
+++ F5074.DevExpressWinforms/MyForm/I_XtraReport/MyXtraReport01.cs
@@ -23,11 +23,14 @@ namespace F5074.DevExpressWinforms.MyForm.I_XtraReport
         private void SimpleButton1_Click(object sender, EventArgs e)
         {
             Report01 report = new Report01();
+            //report.PrintingSystem.Document.AutoFitToPagesWidth = 3;
             ReportPrintTool pt = new ReportPrintTool(report);
             // https://www.devexpress.com/Support/Center/Question/Details/Q296340/position-of-the-report-on-the-screen
+            // https://www.devexpress.com/Support/Center/Question/Details/T587198/how-to-zoom-a-report-in-the-documentviewer-to-fit-into-the-page-width
             pt.PreviewForm.StartPosition = FormStartPosition.CenterScreen;
             pt.PreviewForm.SaveState = false;
             pt.PreviewForm.SetDesktopBounds(0, 0, 1000, 900);
+            pt.PreviewForm.PrintControl.ExecCommand(DevExpress.XtraPrinting.PrintingSystemCommand.ZoomToPageWidth, null);
             //pt.ShowPreview();
             pt.ShowRibbonPreviewDialog();
             //pt.ShowRibbonPreview();
