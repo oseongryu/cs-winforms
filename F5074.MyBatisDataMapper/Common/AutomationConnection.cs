f01efcd4c9ed05213bb4e7268dd933ed68e5dd2e
f5074 <oseongryu@gmail.com>
2019-06-26 11:10:08 +0900

    Settings Modify location of mdf file

--- F5074.MyBatisDataMapper/Common/AutomationConnection.cs
+++ F5074.MyBatisDataMapper/Common/AutomationConnection.cs
@@ -4,6 +4,7 @@ using IBatisNet.DataMapper.Configuration;
 using IBatisNet.DataMapper.SessionStore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,7 +32,13 @@ namespace F5074.MyBatisDataMapper.Common
 
 
                     mapper.SessionStore = new HybridWebThreadSessionStore(mapper.Id);
-                    mapper.DataSource.ConnectionString = @"Server=(localdb)\MSSQLLocalDB; Integrated Security=true ;AttachDbFileName=C:\Users\itcomm\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\mssqllocaldb\AutomationDB.mdf;";
+                    //mapper.DataSource.ConnectionString = @"Server=(localdb)\MSSQLLocalDB; Integrated Security=true ;AttachDbFileName=C:\Users\f5074\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\mssqllocaldb\AutomationDB.mdf;";
+                    //string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+                    //mapper.DataSource.ConnectionString = string.Format(@"Server=(localdb)\MSSQLLocalDB; Integrated Security=true ;AttachDbFileName={0};", Path.GetFullPath(Path.Combine(projectDirectory, @"..\\Lib\\AutomationDB.mdf")));
+                    mapper.DataSource.ConnectionString = string.Format(@"Server=(localdb)\MSSQLLocalDB; Integrated Security=true ;AttachDbFileName={0};", AppDomain.CurrentDomain.BaseDirectory + "Lib\\AutomationDB.mdf");
+
+
+
 
                     return mapper;
                 }
