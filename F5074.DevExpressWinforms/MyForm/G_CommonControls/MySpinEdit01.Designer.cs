ee05e8d0c843ffcfdd8df73f33047d3e0b4f4f75
f5074 <oseongryu@gmail.com>
2019-07-11 14:00:06 +0900

    Add SpinEdit

---
+++ F5074.DevExpressWinforms/MyForm/G_CommonControls/MySpinEdit01.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace F5074.DevExpressWinforms.MyForm.G_CommonControls
+{
+    partial class MySpinEdit01
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spinEdit1 = new DevExpress.XtraEditors.SpinEdit();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // spinEdit1
+            // 
+            this.spinEdit1.EditValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.spinEdit1.Location = new System.Drawing.Point(78, 160);
+            this.spinEdit1.Name = "spinEdit1";
+            this.spinEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.spinEdit1.Size = new System.Drawing.Size(194, 20);
+            this.spinEdit1.TabIndex = 0;
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(310, 172);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(100, 20);
+            this.textEdit1.TabIndex = 1;
+            // 
+            // MySpinEdit01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.spinEdit1);
+            this.Name = "MySpinEdit01";
+            this.Size = new System.Drawing.Size(509, 359);
+            ((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SpinEdit spinEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+    }
+}
---
+++ F5074.DevExpressWinforms/MyForm/G_CommonControls/MySpinEdit01.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.DevExpressWinforms.MyForm.G_CommonControls
+{
+    public partial class MySpinEdit01 : UserControl
+    {
+        public MySpinEdit01()
+        {
+            InitializeComponent();
+            //this.spinEdit1.EditValueChanged += SpinEdit1_EditValueChanged;
+            this.spinEdit1.ValueChanged += SpinEdit1_ValueChanged;
+            this.spinEdit1.Properties.EditValueChangedFiringMode = DevExpress.XtraEditors.Controls.EditValueChangedFiringMode.Default;
+            this.spinEdit1.Properties.EditValueChangedDelay = 0;
+            this.spinEdit1.Properties.MaxValue = 9999999;
+            this.spinEdit1.Properties.MaxLength = 8;
+            this.spinEdit1.Properties.Increment = 1;
+            this.spinEdit1.Properties.AllowNullInput = DevExpress.Utils.DefaultBoolean.False;
+            this.spinEdit1.Properties.EditMask = @"#,###,##0";
+            this.spinEdit1.Properties.DisplayFormat.FormatString = @"#,###,##0";
+            this.spinEdit1.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom;
+
+
+            textEdit1.Properties.Mask.EditMask = @"#,###,##0";
+            textEdit1.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.Numeric;
+            textEdit1.Properties.Mask.UseMaskAsDisplayFormat = true;
+            textEdit1.Properties.DisplayFormat.FormatString = @"#,###,##0";
+            textEdit1.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom;
+
+            //textEdit1.Properties.EditValueChangedFiringMode = EditValueChangedFiringMode.Default;
+            //textEdit1.Properties.EditValueChangedDelay = 0;   
+        }
+
+        private void SpinEdit1_ValueChanged(object sender, EventArgs e)
+        {
+            this.textEdit1.EditValue = this.spinEdit1.EditValue;
+
+        }
+
+        //private void SpinEdit1_EditValueChanged(object sender, EventArgs e)
+        //{
+        //    this.textEdit1.EditValue = this.spinEdit1.EditValue;
+        //}
+    }
+}
