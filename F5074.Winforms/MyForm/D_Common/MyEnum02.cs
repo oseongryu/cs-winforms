f212765c188c4174242cf5fdb10c2b8eee706257
f5074 <oseongryu@gmail.com>
2019-07-12 10:21:26 +0900

    -

--- F5074.Winforms/MyForm/D_Common/MyEnum02.cs
+++ F5074.Winforms/MyForm/D_Common/MyEnum02.cs
@@ -17,32 +17,32 @@ namespace F5074.Winforms.MyForm.D_Common
             InitializeComponent();
             MakeSentences();
         }
-
-        enum ma3
+        private enum DbColumnIndex
         {
-            AUFNR
-,VORNR
-,KTEXT
-,WTEXT
-,WDATE
-,LIFNR
-,NAME1
-,CKEY
+            LOT_ID, PROCESS_NAME, ITEM_CD, ITEM_NM, ITEM_SEQ, VALUE_SEQ, T_VALUE
+        };
 
-
-
-        }
+        string[] oHeaderText =
+        {
+              "LOT ID"
+            , "공정명"
+            , "검사 항목 코드"
+            , "검사 항목명"
+            , "검사 횟수"
+            , "값 순서"
+            , "값"
+        };
 
         private void MakeSentences()
         {
             StringBuilder text = new StringBuilder(this.richTextBox1.Text);
-            for (int x = 0; x < Enum.GetNames(typeof(ma3)).Length; x++)
+            for (int x = 0; x < Enum.GetNames(typeof(DbColumnIndex)).Length; x++)
             {
-                Console.WriteLine(Enum.GetNames(typeof(ma3))[x].ToString());
+                Console.WriteLine(Enum.GetNames(typeof(DbColumnIndex))[x].ToString());
 
                 //text.AppendFormat("{0}public string {1} {2}", Environment.NewLine, Enum.GetNames(typeof(ma3))[x].ToString(), "{ get; set; }");
                 //text.AppendFormat("{0}{1} = rdr.GetValue(rdr.GetOrdinal(\"{1}\")),", Environment.NewLine, Enum.GetNames(typeof(ma3))[x].ToString());
-                text.AppendFormat("{0}tempVo.{1} = dtTable3.Rows[0][\"{1}\"].ToString();", Environment.NewLine, Enum.GetNames(typeof(ma3))[x].ToString());
+                text.AppendFormat("{0}{5} AllowEdit = false, Visble= {4}, HAlignment= {3}, Caption= \"{2}\", FieldName=\"{1}\" {6} ", Environment.NewLine, Enum.GetNames(typeof(DbColumnIndex))[x].ToString(), oHeaderText[x], "HorzAlignment.Center", "true", "columnList.Add(new GridControlColumn() {", "});", "false");
 
                 richTextBox1.Text = text.ToString();
                 richTextBox1.SelectionStart = richTextBox1.Text.Length;
