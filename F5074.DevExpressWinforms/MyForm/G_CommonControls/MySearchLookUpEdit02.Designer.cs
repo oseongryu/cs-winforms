c8c9d91c9266d8bfb7ba9fac2d3cacf337234174
f5074 <oseongryu@gmail.com>
2019-10-29 17:55:47 +0900

    SearchLookUpEdit

---
+++ F5074.DevExpressWinforms/MyForm/G_CommonControls/MySearchLookUpEdit02.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace F5074.DevExpressWinforms.MyForm.G_CommonControls
+{
+    partial class MySearchLookUpEdit02
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lookUpEdit1 = new DevExpress.XtraEditors.LookUpEdit();
+            this.spinEdit1 = new DevExpress.XtraEditors.SpinEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.lookUpEdit2 = new DevExpress.XtraEditors.LookUpEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit2.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lookUpEdit1
+            // 
+            this.lookUpEdit1.Location = new System.Drawing.Point(38, 61);
+            this.lookUpEdit1.Name = "lookUpEdit1";
+            this.lookUpEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit1.Size = new System.Drawing.Size(176, 20);
+            this.lookUpEdit1.TabIndex = 1;
+            // 
+            // spinEdit1
+            // 
+            this.spinEdit1.EditValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.spinEdit1.Location = new System.Drawing.Point(38, 280);
+            this.spinEdit1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.spinEdit1.Name = "spinEdit1";
+            this.spinEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.spinEdit1.Properties.MaxValue = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.spinEdit1.Size = new System.Drawing.Size(88, 20);
+            this.spinEdit1.TabIndex = 5;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(131, 279);
+            this.simpleButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(133, 17);
+            this.simpleButton1.TabIndex = 4;
+            this.simpleButton1.Text = "Set EditValue by ID";
+            // 
+            // lookUpEdit2
+            // 
+            this.lookUpEdit2.Location = new System.Drawing.Point(38, 258);
+            this.lookUpEdit2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lookUpEdit2.Name = "lookUpEdit2";
+            this.lookUpEdit2.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lookUpEdit2.Size = new System.Drawing.Size(226, 20);
+            this.lookUpEdit2.TabIndex = 3;
+            // 
+            // MySearchLookUpEdit02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.spinEdit1);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.lookUpEdit2);
+            this.Controls.Add(this.lookUpEdit1);
+            this.Name = "MySearchLookUpEdit02";
+            this.Size = new System.Drawing.Size(959, 545);
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lookUpEdit2.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit1;
+        private DevExpress.XtraEditors.SpinEdit spinEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.LookUpEdit lookUpEdit2;
+    }
+}
---
+++ F5074.DevExpressWinforms/MyForm/G_CommonControls/MySearchLookUpEdit02.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.DevExpressWinforms.MyForm.G_CommonControls
+{
+    public partial class MySearchLookUpEdit02 : UserControl
+    {
+        public MySearchLookUpEdit02()
+        {
+            InitializeComponent();
+            InitLookUpEdit();
+            if ( datasource.Count == 1 ) lookUpEdit1.EditValue = datasource[ 0 ].ID;
+
+
+
+            lookUpEdit2.Properties.ValueMember = "ID";
+            lookUpEdit2.Properties.DisplayMember = "Text";
+            lookUpEdit2.Properties.DataSource = DataStorage.GetData();
+            lookUpEdit2.EditValue = 2;
+            simpleButton1.Click += SimpleButton1_Click;
+        }
+
+        private void SimpleButton1_Click( object sender, EventArgs e )
+        {
+            lookUpEdit2.EditValue = (int) spinEdit1.Value;
+
+        }
+
+        static class DataStorage
+        {
+            public static IEnumerable<DataObject> GetData()
+            {
+                for ( int i = 0; i < 10; i++ )
+                    yield return new DataObject() { ID = i, Text = ( (char) ( 'A' + i ) ).ToString() };
+            }
+            public class DataObject
+            {
+                public int ID { get; set; }
+                public string Text { get; set; }
+            }
+        }
+
+        private List<Account> datasource;
+        private void InitLookUpEdit()
+        {
+            datasource = new List<Account>();
+            Random random = new Random();
+            datasource.Add( new Account( "P" ) { ID = random.Next( 100 ) } );
+            datasource.Add( new Account( "S" ) { ID = random.Next( 100 ) } );
+            lookUpEdit1.Properties.DataSource = datasource;
+            lookUpEdit1.Properties.DisplayMember = "Name";
+            lookUpEdit1.Properties.ValueMember = "ID";
+        }
+
+        public class Account
+        {
+            public Account( string name )
+            {
+                Name = name;
+            }
+            private int _ID;
+            private string _Name;
+            public string Name
+            {
+                get { return _Name; }
+                set
+                {
+                    _Name = value;
+                }
+            }
+            public int ID
+            {
+                get { return _ID; }
+                set
+                {
+                    _ID = value;
+                }
+            }
+        }
+    }
+}
