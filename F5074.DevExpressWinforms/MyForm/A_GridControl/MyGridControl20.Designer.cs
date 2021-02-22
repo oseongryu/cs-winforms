4e796a5f80d781b8e33068aab15ac0b2a47360b8
f5074 <oseongryu@gmail.com>
2021-02-22 11:39:10 +0900

    [GridControl] Grouping & Filtering

---
+++ F5074.DevExpressWinforms/MyForm/A_GridControl/MyGridControl20.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace F5074.DevExpressWinforms.MyForm.A_GridControl
+{
+    partial class MyGridControl20
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.AllowRestoreSelectionAndFocusedRow = DevExpress.Utils.DefaultBoolean.True;
+            this.gridControl1.Location = new System.Drawing.Point(28, 24);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.ShowOnlyPredefinedDetails = true;
+            this.gridControl1.Size = new System.Drawing.Size(729, 182);
+            this.gridControl1.TabIndex = 16;
+            this.gridControl1.UseDisabledStatePainter = false;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Appearance.FocusedRow.BackColor = System.Drawing.Color.Red;
+            this.gridView1.Appearance.FocusedRow.Options.UseBackColor = true;
+            this.gridView1.Appearance.SelectedRow.BackColor = System.Drawing.Color.Red;
+            this.gridView1.Appearance.SelectedRow.Options.UseBackColor = true;
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(51, 230);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(87, 21);
+            this.simpleButton1.TabIndex = 17;
+            this.simpleButton1.Text = "Click";
+            // 
+            // MyGridControl20
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "MyGridControl20";
+            this.Size = new System.Drawing.Size(1270, 409);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+    }
+}
---
+++ F5074.DevExpressWinforms/MyForm/A_GridControl/MyGridControl20.cs
@@ -0,0 +1,91 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraGrid.Views.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F5074.DevExpressWinforms.MyForm.A_GridControl
+{
+    public partial class MyGridControl20 : UserControl
+    {
+        public MyGridControl20()
+        {
+            InitializeComponent();
+            gridControl1.DataSource = GetData(10);
+            gridView1.Columns["ID"].GroupIndex = 0;
+            gridView1.Columns["Name"].GroupIndex = 1;
+            gridView1.Columns["Info"].GroupIndex = 2;
+            this.simpleButton1.Click += SimpleButton1_Click;
+            this.gridView1.StartGrouping += GridView1_StartGrouping;
+        }
+
+        private void GridView1_StartGrouping(object sender, EventArgs e)
+        {
+            if (gridView1.GroupedColumns[0] != null
+            && (gridView1.GroupedColumns[0].FieldName == "ID"))
+            {
+                gridView1.ClearGrouping();
+                return;
+            }
+
+            if (gridView1.GroupedColumns[1] != null)
+            {
+                String fieldName = gridView1.GroupedColumns[1].FieldName;
+                gridView1.ClearGrouping();
+                gridView1.Columns[fieldName].GroupIndex = 0;
+            }
+        }
+
+        private void SimpleButton1_Click(object sender, EventArgs e)
+        {
+            if (!gridView1.IsGroupRow(gridView1.FocusedRowHandle))
+                return;
+            int index = gridView1.GetRowLevel(gridView1.FocusedRowHandle);
+            string fieldName = gridView1.GroupedColumns[index].FieldName;
+            XtraMessageBox.Show(fieldName);
+
+            DataTable dt = GetFilteredData(gridView1).ToTable();
+
+        }
+
+        DataTable GetData(int count)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Quantity", typeof(int));
+            dt.Columns.Add("Info", typeof(string));
+            Random r = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                dt.Rows.Add(i, "Name" + i, r.Next(100), "Info" + i);
+            }
+            return dt;
+        }
+
+
+        /// <summary>
+        /// GetFilteredData
+        /// </summary>
+        /// <param name="view"></param>
+        /// <returns></returns>
+        public static DataView GetFilteredData(ColumnView view)
+        {
+            if (view == null) return null;
+            if (view.ActiveFilter == null || !view.ActiveFilterEnabled
+                || view.ActiveFilter.Expression == "")
+                return view.DataSource as DataView;
+
+            DataTable table = ((DataView)view.DataSource).Table;
+            DataView filteredDataView = new DataView(table);
+            filteredDataView.RowFilter = DevExpress.Data.Filtering.CriteriaToWhereClauseHelper.GetDataSetWhere(view.ActiveFilterCriteria);
+            return filteredDataView;
+        }
+    }
+}
